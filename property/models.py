2ea8886020f72a6e76596843af0e07f1414ea101
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-11-04 08:55:46 +0300

    dry related_name

--- property/models.py
+++ property/models.py
@@ -85,11 +85,11 @@ class Owner(models.Model):
     name = models.CharField('ФИО владельца', max_length=200, db_index=True)
     phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
     pure_phone = PhoneNumberField('Нормализованный Номер владельца',
-                                        blank=True, db_index=True
-                                        )
+                                  blank=True, db_index=True
+                                  )
     owned_by = models.ManyToManyField(Flat,
                                       verbose_name='Квартиры в собственности',
-                                      related_name='flat_owners')
+                                      related_name='owners')
 
     def __str__(self):
         return self.owner
